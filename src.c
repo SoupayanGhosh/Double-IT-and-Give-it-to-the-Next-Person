383042cdd9650db890745b34bb64281622a6a93a
SoupayanGhosh <97727814+SoupayanGhosh@users.noreply.github.com>
2023-05-13 17:09:33 +0530

    SRC.c

---
+++ src.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int N;
+    printf("Enter a number:");
+    scanf("%d", &N);
+    printf("Double it and give it to the next person: %d\n", N * 2);
+    return 0;
+}
